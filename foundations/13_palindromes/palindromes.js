98e44efe152c764dd60cdf909f27ba7c8db0a10c
Joby Martin <jobymartin4th@gmail.com>
2026-04-14 09:25:25 -0400

    Complete exercises 12-16

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  let cleanWord = word.toLowerCase().replace(/[^a-z0-9]/g, '')
+  let reversedWord = cleanWord.split('').reverse().join('')
+  return cleanWord === reversedWord
 };
 
 // Do not edit below this line
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+  count = parseInt(count)
+  if (count < 0) return "OOPS"
+  if (count === 0) return 0
+  if (count === 1) return 1
 
-};
+  let a = 0
+  let b = 1
+  for (let i = 2; i <= count; i++) {
+    let temp = a + b
+    a = b
+    b = temp
+  }
+  return b
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
